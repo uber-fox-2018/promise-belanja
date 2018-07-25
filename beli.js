618db6d848660e6f0ab1bb56d8e613faadafa6ef
Susan Nio <susan.wen88@gmail.com>
2018-07-25 19:26:50 +0700

    Adding beli.js

--- beli.js
+++ beli.js
@@ -1,15 +1,12 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
+function beli(uang, obj){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+    return new Promise((resolve, reject)=>{
+        if (kembalian > 0) {
+            resolve(kembalian)
+        }else{
+            reject(`uang gak cukup nih buat beli ${obj.item}, uang saya hanya ${uang}`)
+        }
+    })
 }
 
 module.exports = beli;
